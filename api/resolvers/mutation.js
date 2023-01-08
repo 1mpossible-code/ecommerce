5eeb1dc5ac69080e81c4675e02b15abadbc2b692
Max <linme00p@gmail.com>
2023-01-08 00:51:33 -0500

    Create CRUD implementation for Product model. Install graphql-iso-date to implement DateTime type in GraphQL. Update schema to use DateTime and CRUD methods.

--- api/resolvers/mutation.js
+++ api/resolvers/mutation.js
@@ -1,7 +1,25 @@
-const models = require('../models');
-
 module.exports = {
-    createProduct: async (_, {name, description, price, quantity}) => {
+    createProduct: async (_, {name, description, price, quantity}, {models}) => {
         return await models.Product.create({name, description, price, quantity});
+    },
+    updateProduct: async (_, {id, name, description, price, quantity}, {models}) => {
+        return await models.Product.findOneAndUpdate({_id: id},
+            {
+                $set: {
+                    name,
+                    description,
+                    price,
+                    quantity,
+                },
+            },
+            {new: true});
+    },
+    deleteProduct: async (_, {id}, {models}) => {
+        try {
+            await models.Product.findByIdAndDelete(id);
+            return true;
+        } catch (e) {
+            return false;
+        }
     },
 };
--- api/schema.js
+++ api/schema.js
@@ -1,12 +1,15 @@
 const {gql} = require('apollo-server-express');
 
 module.exports = gql`
+    scalar DateTime
     type Product {
         id: ID!
         name: String!
         description: String!
         price: Float!
         quantity: Int!
+        createdAt: DateTime!
+        updatedAt: DateTime!
     }
     type Query {
         hello: String
@@ -15,5 +18,7 @@ module.exports = gql`
     }
     type Mutation {
         createProduct(name: String!, description: String!, price: Float!, quantity: Int!): Product!
+        updateProduct(id: ID!, name: String, description: String, price: Float, quantity: Int): Product!
+        deleteProduct(id: ID!): Boolean!
     }
 `;
