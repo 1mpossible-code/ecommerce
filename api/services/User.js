524009838b89e54be42dafccbb20a86e5a1cf81c
Max <linme00p@gmail.com>
2023-01-14 22:54:47 -0500

    Fix of the role checking User service.

--- api/services/User.js
+++ api/services/User.js
@@ -1,11 +1,12 @@
 module.exports = {
     isAdmin: async (models, user) => {
-        return await models.User.findById(user.id)?.role === 'admin';
+        const userObj = await models.User.findById(user.id);
+        return userObj.role === 'admin';
     },
     isManager: async (models, user) => {
         // Check if user is manager or admin and return true if so
-        const role = await models.User.findById(user.id)?.role;
-        return role === 'manager' || role === 'admin';
+        const userObj = await models.User.findById(user.id);
+        return userObj.role === 'manager' || userObj.role === 'admin';
     },
     isAuthenticated: (user) => {
         return user;
