785204ea3197f706359aeb775d0bdf1a9fa9f434
Max <linme00p@gmail.com>
2023-01-19 22:59:32 -0500

    Replace state handling form with Formik (without validation).

--- web/src/pages/Signup/index.tsx
+++ web/src/pages/Signup/index.tsx
@@ -1,6 +1,7 @@
-import React, {ChangeEvent, FormEvent, useEffect, useState} from 'react';
+import React from 'react';
 import {gql, useMutation} from '@apollo/client';
 import {useNavigate} from 'react-router-dom';
+import {ErrorMessage, Field, Form, Formik} from 'formik';
 
 const SIGNUP = gql`
 mutation Mutation($email: String!, $firstName: String!, $lastName: String!, $password: String!) {
@@ -8,28 +9,8 @@ mutation Mutation($email: String!, $firstName: String!, $lastName: String!, $pas
 }
 `;
 
-
 const Signup = () => {
-    type Values = {
-        email: string;
-        firstName: string;
-        lastName: string;
-        password: string;
-        confirmPassword: string;
-    };
-
-    const [values, setValues] = useState<Values>({
-        email: '',
-        firstName: '',
-        lastName: '',
-        password: '',
-        confirmPassword: '',
-    });
-
-    const [passwordsDoNotMatch, setPasswordsDoNotMatch] = useState<boolean | 'not touched'>('not touched');
-
     const navigate = useNavigate();
-
 
     const [signup, {loading, error}] = useMutation(SIGNUP, {
         onCompleted: (data) => {
@@ -38,78 +19,49 @@ const Signup = () => {
         },
     });
 
-    useEffect(() => {
-        if (error) {
-            setValues({
-                ...values,
-                password: '',
-                confirmPassword: '',
-            });
-        }
-    }, [error]);
-
-    useEffect(() => {
-        if (values.password !== values.confirmPassword) {
-            setPasswordsDoNotMatch(true);
-        } else {
-            setPasswordsDoNotMatch(false);
-        }
-    }, [values.password, values.confirmPassword]);
-
-
-    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setValues({
-            ...values,
-            [e.target.name]: e.target.value,
-        });
-    };
-
-    const onSubmit = async (e: FormEvent) => {
-        e.preventDefault();
-        const {email, firstName, lastName, password, confirmPassword} = values;
-        if (password !== confirmPassword) {
-            return;
-        }
-
-        await signup({variables: {email, firstName, lastName, password}});
-        navigate('/');
-    };
-
     return <div>
         <h1>Signup</h1>
         {error && <div>{error.graphQLErrors.map(message => (
             <div key={message.message}>{message.message}</div>
         ))}</div>}
-        <form onSubmit={onSubmit}>
-            <div>
-                <label htmlFor='email'>Email</label>
-                <input type='text' placeholder='Email' name='email' onChange={onChange} value={values.email} />
-            </div>
-            <div>
-                <label htmlFor='first-name'>First name</label>
-                <input type='text' placeholder='First name' name='firstName' onChange={onChange}
-                       value={values.firstName} />
-            </div>
-            <div>
-                <label htmlFor='last-name'>Last name</label>
-                <input type='text' placeholder='Last name' name='lastName' onChange={onChange}
-                       value={values.lastName} />
-            </div>
-            <div>
-                <label htmlFor='password'>Password</label>
-                <input type='password' placeholder='Password' name='password' onChange={onChange}
-                       value={values.password} />
-            </div>
-            <div>
-                {passwordsDoNotMatch && passwordsDoNotMatch !== 'not touched' && <div>Passwords do not match</div>}
-                <label htmlFor='confirm-password'>Confirm password</label>
-                <input type='password' placeholder='Confirm password' name='confirmPassword'
-                       onChange={onChange} value={values.confirmPassword} />
-            </div>
-            <div>
-                <input type='submit' name='submit' placeholder='Submit' />
-            </div>
-        </form>
+        <Formik initialValues={{email: '', firstName: '', lastName: '', password: '', confirmPassword: ''}} onSubmit={
+            async (values) => {
+                const {email, firstName, lastName, password} = values;
+                await signup({variables: {email, firstName, lastName, password}});
+                navigate('/');
+            }
+        }>
+            {({isSubmitting}) => (
+                <Form>
+                    <div>
+                        <label htmlFor='email'>Email</label>
+                        <Field type='email' name='email' placeholder='Email' />
+                        <ErrorMessage name={'email'} component={'div'} />
+                    </div>
+                    <div>
+                        <label htmlFor='firstName'>First Name</label>
+                        <Field type='text' name='firstName' placeholder='First Name' />
+                        <ErrorMessage name={'firstName'} component={'div'} />
+                    </div>
+                    <div>
+                        <label htmlFor='lastName'>Last Name</label>
+                        <Field type='text' name='lastName' placeholder='Last Name' />
+                        <ErrorMessage name={'lastName'} component={'div'} />
+                    </div>
+                    <div>
+                        <label htmlFor='password'>Password</label>
+                        <Field type='password' name='password' placeholder='Password' />
+                        <ErrorMessage name={'password'} component={'div'} />
+                    </div>
+                    <div>
+                        <label htmlFor='confirmPassword'>Confirm Password</label>
+                        <Field type='password' name='confirmPassword' placeholder='Confirm Password' />
+                        <ErrorMessage name={'confirmPassword'} component={'div'} />
+                    </div>
+                    <button type='submit' disabled={isSubmitting}>Submit</button>
+                </Form>
+            )}
+        </Formik>
     </div>;
 };
 
