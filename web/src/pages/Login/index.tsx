1cfb15b0d641c6480c25b08a2c97446dcf2741d4
Max <linme00p@gmail.com>
2023-01-20 22:50:16 -0500

    Rewrite Login page using Formik, react-spinner, and Yup.

--- web/src/pages/Login/index.tsx
+++ web/src/pages/Login/index.tsx
@@ -1,6 +1,9 @@
-import React, {ChangeEvent, FormEvent, useState} from 'react';
+import React from 'react';
 import {gql, useMutation} from '@apollo/client';
 import {useNavigate} from 'react-router-dom';
+import {ErrorMessage, Field, Form, Formik} from 'formik';
+import * as Yup from 'yup';
+import {HashLoader} from 'react-spinners';
 
 const SIGNIN = gql`
 mutation Signin($email: String!, $password: String!) {
@@ -8,18 +11,15 @@ mutation Signin($email: String!, $password: String!) {
 }
 `;
 
+const SigninSchema = Yup.object().shape({
+    email: Yup.string().email('Invalid email').max(80, 'Email cannot exceed 80 symbols').required('Required'),
+    password: Yup.string().min(8, 'Password should be at least 8 symbols').max(80, 'Password cannot be longer than 80 symbols').required('Required'),
+});
+
 const Login = () => {
-    const [values, setValues] = useState({});
     const navigate = useNavigate();
 
-    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setValues({
-            ...values,
-            [e.target.name]: e.target.value,
-        });
-    };
-
-    const [signIn] = useMutation(SIGNIN, {
+    const [signin, {loading, error}] = useMutation(SIGNIN, {
             onCompleted: (data) => {
                 localStorage.setItem('token', data.signin);
                 navigate('/');
@@ -27,25 +27,37 @@ const Login = () => {
         },
     );
 
-    const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        await signIn({variables: values});
-    };
+    if (loading) return <HashLoader color='#000' loading={loading} size={150} />;
 
     return (
         <div>
             <h1>Login</h1>
-            <form onSubmit={onSubmit}>
-                <div>
-                    <input type='text' placeholder='Email' name='email' onChange={onChange} />
-                </div>
-                <div>
-                    <input type='password' placeholder='Password' name='password' onChange={onChange} />
-                </div>
-                <div>
-                    <input type='submit' name='submit' placeholder='Submit' />
-                </div>
-            </form>
+            {error && <div>{error.graphQLErrors.map(message => (
+                <div key={message.message}>{message.message}</div>
+            ))}</div>}
+            <Formik initialValues={{email: '', password: ''}} onSubmit={
+                async (values) => {
+                    const {email, password} = values;
+                    await signin({variables: {email, password}});
+                    navigate('/');
+                }
+            } validationSchema={SigninSchema}>
+                {({isSubmitting}) => (
+                    <Form>
+                        <div>
+                            <label htmlFor='email'>Email</label>
+                            <Field type='email' name='email' placeholder='Email' />
+                            <ErrorMessage name={'email'} component={'div'} />
+                        </div>
+                        <div>
+                            <label htmlFor='password'>Password</label>
+                            <Field type='password' name='password' placeholder='Password' />
+                            <ErrorMessage name={'password'} component={'div'} />
+                        </div>
+                        <button type='submit' disabled={isSubmitting}>Submit</button>
+                    </Form>
+                )}
+            </Formik>
         </div>
     );
 };
